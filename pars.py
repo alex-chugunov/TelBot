69281897e0bdf0fe3503b082364a161da24e2cc3
Alexey Chugunov <57045051+alex-chugunov@users.noreply.github.com>
2022-04-13 13:31:48 +0300

    4 заход
    Положил бд, скрины в data. Теперь новость про биткоин. Удалил лишнее

--- pars.py
+++ pars.py
@@ -1,66 +1,18 @@
-# https://github.com/Ynxela/rbc_parser
 from bs4 import BeautifulSoup
-from links import rbk
+from links import url_pars
 import requests
-import json
-
-URL = rbk
 
 
-def get_soup(url):
-    r = requests.get(url).text
+def get_soup(url_pars):
+    r = requests.get(url_pars).text
     return BeautifulSoup(r, 'lxml')
 
 
-def save_json(data):
-    with open('rbk_data.json', "w") as file:
-        json.dump(data, file, ensure_ascii=False, indent=4)
-
-
 def collect():
-    news_data = {}
-
-    soup = get_soup(URL)
-
-    # Получаем все ссылки на новости
-    news_links = soup.find('div', class_='main').find_all('a', {'class': ['main__big__link', 'main__feed__link']})
-
-    link = news_links[1].get('href').split('?')[0]
-    name = link
-    news_data[name] = {}
-    soup = get_soup(link)
-
-    # Переходим на страницу для дальнейшенго парсинга
-    article = soup.find('div', class_='article')
-    category = article.find('a', class_='article__header__category')
-    date = article.find('span', class_='article__header__date').get('content').replace('T', ' ').split('+')[0]
-    title = article.find('span', class_='js-slide-title')
-    image = article.find('div', class_='article__main-image')
-    article_paragraphs = article.find_all('p')
-    article_text = ''
-    for paragraph in article_paragraphs:
-        article_text += paragraph.text
-
-    # Заполняем полученными данными news_data
-    news_data[name]['link'] = link
-    news_data[name]['date'] = date
-    news_data[name]['text'] = article_text.replace('\xa0', '').replace('\n', '').replace('\r', '')
-
-    try:
-        news_data[name]['title'] = title.text
-    except AttributeError:
-        news_data[name]['title'] = 'Без заголовка'
-    try:
-        news_data[name]['category'] = category.text.replace('\n', '')
-    except AttributeError:
-        news_data[name]['category'] = 'Без категории'
-    try:
-        news_data[name]['image'] = image.find('img').get('src')
-    except AttributeError:
-        news_data[name]['image'] = 'Без обложки'
-
-    save_json(news_data)
-    text = news_data[name]['text']
-    text = text[:300]
-    text = text.rpartition('.')[0]+"." + f"\n{news_data[name]['link']}"
+    soup = get_soup(url_pars)
+    items = soup.find('div', class_='row news-item start-xs')
+    itemTitle = items.find('a', class_='title').text
+    itemLink = items.find('a', href=True)
+    itemLink = 'https://cryptonews.net' + itemLink['href']
+    text = itemTitle + f"\n{itemLink}"
     return text
--- test_mybot.py
+++ test_mybot.py
@@ -5,19 +5,7 @@ from pars import collect
 
 file_id = 'AwACAgIAAxkBAAIEimJVRxRswV4_ihP-2o_tOmmT1dDKAALvFAACwtmpSsul1wckvTCxIwQ'
 msg = 229133725
-# Питон не воспринимает объект message, пришлось отдельно тестить получение курса
-message = {'content_type': 'text', 'message_id': 1177,
-           'from_user': {'id': 229133725, 'first_name': 'Alexey', 'username': None, 'last_name': None},
-           'date': 1649756649,
-           'chat': {'type': 'private', 'last_name': None, 'first_name': 'Alexey', 'username': None, 'id': 229133725,
-                    'title': None, 'all_members_are_administrators': None}, 'forward_from_chat': None,
-           'forward_from': None, 'forward_date': None, 'reply_to_message': None, 'edit_date': None, 'text': 'Доллар',
-           'entities': None, 'audio': None, 'document': None, 'photo': None, 'sticker': None, 'video': None,
-           'voice': None, 'caption': None, 'contact': None, 'location': None, 'venue': None, 'new_chat_member': None,
-           'left_chat_member': None, 'new_chat_title': None, 'new_chat_photo': None, 'delete_chat_photo': None,
-           'group_chat_created': None, 'supergroup_chat_created': None, 'channel_chat_created': None,
-           'migrate_to_chat_id': None, 'migrate_from_chat_id': None, 'pinned_message': None}
-text = "Минздрав не исключает риска новой волны коронавируса в России, заявил глава ведомства Михаил Мурашко в интервью Наиле Аскер-заде на канале «Россия 24». «Мы видим, что в ряде стран сегодня уже подъем заболеваемости констатируют медицинские работники»,— сказал он.\nhttps://www.rbc.ru/society/12/04/2022/625534079a79472ad719d480"
+text = 'Падение биткоина сменилось консолидацией выше уровня $ 40000\nhttps://cryptonews.net/ru/news/bitcoin/4991037/'
 
 
 class TestMyBot(unittest.TestCase):
